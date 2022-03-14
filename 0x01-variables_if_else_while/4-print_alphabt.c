b41a0f58cdaa9013afd437713867f8251cdd8eb5
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-14 11:39:43 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <ctype.h>
+/**
+ * main - print alphabet in upper and lower case
+ *
+ * Return: 0 if successfully
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q'; ch != 'e')
+		{
+			putchar(tolower(ch));
+		}
+	}
+	return (0);
+}
