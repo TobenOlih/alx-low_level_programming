3c48f96be284a54dad00e1b0b63620f6ae1cb55e
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-15 13:18:10 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Inputs new line
+ *
+ * Return: 0 if exited properly
+*/
+int main(void)
+{
+	int i;
+
+	while (i > 5)
+	{
+		_putchar ('\n');
+		return (0);
+	}
+
+	return (0);
+}
