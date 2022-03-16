7f80e3c0802f7a2a6489f3e6535edeb4c8313db8
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-15 19:11:44 -0700

    change

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * @n: parameter
+ *
+ * Return: 0 if successful
+*/
+int print_last_digit(int n)
+{
+	return (print_last_digit(n));
+}
