1ed81972befbda591c39a6aa85c1beda956bfde9
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-16 12:11:25 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the 100 times table, starting with 0.
+ * @n: parameter iterable
+ * Return: Always 0.
+*/
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n, j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0')
+			}
+			else if (k < 15 || j != 0)
+			{
+				_putchar(',');
+				_putchar('.');
+				_putchar('.');
+				_putchar('.');
+				_putchar(k + '0');
+			}
+			else if (k >= 10 || k < 100)
+			{
+				_putchar(',');
+				_putchar('.');
+				_putchar('.');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			else if (k >= 100)
+			{
+				_putchar(',');
+				_putchar('.');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n')
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ int add(int, int);
 int print_last_digit(int);
 void print_to_98(int n);
 void jack_bauer(void);
+void print_times_table(int n);
