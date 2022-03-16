b52303825bb9aef4035168181a5bf1fbed714626
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-15 20:04:12 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - entry point
+ * @i: parameter
+ * @j: parameter
+ * Return: 0 if successful
+*/
+int add(int i, int j)
+{
+	int r;
+
+	r = i + j;
+	return (r);
+}
