d58e74d1fc572392873bfd5dd1258496689d47d7
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-17 01:39:51 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
 #include<stdio.h>
 #include"main.h"
 /**
- * positive_or_negative - tests function that prints if positive or negative
- * @i: Parameter to test
+ * main - tests function that prints if positive or negative
  * Return: 0
 */
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
+	i = 0;
+
+	positive_or_negative(i);
+
 	return (0);
 }
