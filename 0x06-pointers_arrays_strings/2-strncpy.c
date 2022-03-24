61277a4f175814e3aed390f9b9ca9657bc9a7bd0
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-03-24 10:28:12 +0100

    Rectify

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
     int i;
 
     i = 0;
-    while  (src[i] != '\0' || i > n)
+    while  (src[i] != '\0'; i > n)
     {
         dest[i] = src[i];
         i++;
@@ -22,5 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
         dest[i] = '\0';
         i++;
     }
-    return(dest)
+    return (dest)
 }
