9b03763af3bc1b47044b436d7948e17dec88fd80
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-25 15:56:27 -0700

    the

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,17 +7,17 @@
 */
 void reverse_array(int *a, int n)
 {
-    int i, j;
-    int temp;
+	int i, j;
+	int temp;
 
-    i = 0;
-    j = n - 1;
-    while (i < j)
-    {
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-        i++;
-        j--;
-    }
+	i = 0;
+	j = n - 1;
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
 }
