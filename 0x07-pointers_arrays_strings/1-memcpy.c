16712aea0561ad1628abad7a1af3c34d77fd46c9
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-29 15:25:09 -0700

    copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,14 +6,15 @@
  * @src: source
  * @n: number of bytes to copy
  * Return: pointer to dest
- * 
+ *
 */
-char *_memcpy(char *dest, char *src, unsigned int n){
-    unsigned int i;
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-    return (dest);
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
