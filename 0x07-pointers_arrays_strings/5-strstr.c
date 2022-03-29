ec43b328165737dfef07ee9a179b93da36cd8dce
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-03-30 00:58:39 +0100

    pus

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to be searched
+ * @needle: substring to be located
+ *
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{ 
+    int i, j, k;
+    char *p;
+
+    if (needle[0] == '\0')
+        return (haystack);
+    for (i = 0; haystack[i] != '\0'; i++)
+    {
+        for (j = 0; needle[j] != '\0'; j++)
+        {
+            if (haystack[i + j] != needle[j])
+                break;
+            if (needle[j + 1] == '\0')
+            {
+                p = &haystack[i];
+                return (p);
+            }
+        }
+    }
+    return (0);
+}
