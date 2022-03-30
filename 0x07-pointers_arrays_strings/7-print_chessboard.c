06012ca754cf14c34ec46256968a6b219bab5e49
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-03-30 01:24:50 +0100

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: pointer to the array
+ * @size: size of the array
+ * Return: nothing 
+*/
+void print_chessboard(char (*a)[8])
+{
+    int i, j;
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            printf("%c", a[i][j]);
+        }
+        printf("\n");
+    }
+}
