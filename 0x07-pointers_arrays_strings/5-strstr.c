2c6c355e72d2dc0745b0763fd47795dc5f86cfe2
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-29 17:03:37 -0700

    watter

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,24 +8,28 @@
  * Return: pointer to the beginning of the located substring
  */
 char *_strstr(char *haystack, char *needle)
-{ 
-    int i, j, k;
-    char *p;
+{
+	int i, j, k;
+	char *p;
 
-    if (needle[0] == '\0')
-        return (haystack);
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-            if (needle[j + 1] == '\0')
-            {
-                p = &haystack[i];
-                return (p);
-            }
-        }
-    }
-    return (0);
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+			if (needle[j + 1] == '\0')
+			{
+				p = &haystack[i];
+				return (p);
+			}
+		}
+	}
+	return (0);
 }
