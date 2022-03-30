acde59ca8f5086e49ea9abaee7c679dcc1a45dcb
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-29 17:18:27 -0700

    com

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,23 +12,16 @@ char *_strstr(char *haystack, char *needle)
 	int i, j, k;
 	char *p;
 
-	if (needle[0] == '\0')
-	{
-		return (haystack);
-	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
-			{
 				break;
-			}
+			
 			if (needle[j + 1] == '\0')
-			{
 				p = &haystack[i];
 				return (p);
-			}
 		}
 	}
 	return (0);
