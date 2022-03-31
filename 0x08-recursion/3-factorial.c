4cfcf0e7f4cf4272b648ea522f6546def436ebf4
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-04-01 00:26:28 +0100

    swing

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a number
+ * @n: number to check
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+    if (n == 0)
+        return (1);
+    return (n * factorial(n - 1));
+}
