8fc0177f901ff97f491a1b94d8fddca1ede2a00c
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-03-31 23:47:06 +0100

    forn

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include"main.h"
 /**
- * puts_recursion - prints a string, followed by a new line,
+ * _puts_recursion - prints a string, followed by a new line,
  * using recursion
  * @s: string to print
  * Return: void
 */
-void puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
