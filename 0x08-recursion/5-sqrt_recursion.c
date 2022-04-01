305a8ff9ae9bbf82f1fadaf9424dde2b28945c94
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-04-01 01:04:53 +0100

    sqt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the value of x raised to the power of y
+ * @n: base
+ * Return: x raised to the power of y
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+    return(-1);
+    if (n == 0)
+    return(0);
+    if (n == 1)
+    return(1);
+    return(_sqrt_recursion(n - 1) + 1);
+}
