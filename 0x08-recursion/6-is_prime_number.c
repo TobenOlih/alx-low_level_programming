114555c60be906cd60b67afb06cb0c2bfeef5740
TobenOlih <tobechukwuolih@yahoo.com>
2022-03-31 17:24:13 -0700

    pap

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,14 +6,14 @@
 */
 int is_prime_number(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 1)
-        return (0);
-    for (i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-            return (0);
-    }
-    return (1);
+	if (n <= 1)
+		return (0);
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
 }
