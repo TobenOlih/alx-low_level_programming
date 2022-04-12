7c7c41db062614518be8339ca23cf0b75697c2cf
TobenOlih <95124507+TobenOlih@users.noreply.github.com>
2022-04-12 10:20:48 +0100

    First

---
+++ 0x08-recursion/Trap.c
@@ -0,0 +1,6 @@
+#include<main.h>
+/**
+ * prime_number - returns 1 if n is a prime number, 0 otherwise
+ * @n: number to check
+ * Return: 1 if n is a prime number, 0 otherwise
+*/
---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define Size 1024
+
+#endif
